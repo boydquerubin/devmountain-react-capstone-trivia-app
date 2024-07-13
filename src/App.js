74d04fbc45a73497a6fc2a87d51e7fdfff6da155
Boyd Querubin <boydisaacq@gmail.com>
2024-07-13 08:56:10 -0600

    added logo

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Create from "./pages/Create";
 import Update from "./pages/Update";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import logo from "./assets/logo.png"; // Import the logo
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
         <div className="nav-links">
-          <h1>Rubyx Qube</h1>
+          <Link to="/" className="navbar-brand mb-0 h1">
+            <img
+              src={logo}
+              width="60"
+              height="60"
+              alt="Rubyx Qube Logo"
+              className="logo"
+            />
+            <h1>Rubyx Qube</h1>
+          </Link>
+
           <Link to="/">Home</Link>
           <Link to="/create">Create New Category</Link>
         </div>
