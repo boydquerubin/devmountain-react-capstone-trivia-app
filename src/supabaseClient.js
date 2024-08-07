9b42625d1fef89f4150203af1317a03eec638941
Boyd Querubin <boydisaacq@gmail.com>
2024-08-06 18:03:23 -0600

    changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key are required.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
